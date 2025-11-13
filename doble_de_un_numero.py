b91b6b591de5c7a4c0aeea915d44d738ef2cb9f6
stanleycomp <stanleycomputer@hotmail.com>
2025-11-13 00:00:04 -0400

    s

---
+++ doble_de_un_numero.py
@@ -0,0 +1,3 @@
+numero=input("ingrese un numero para calcular su dobleb")
+doble=int(numero)*2
+print ("el doble de ",numero," es ", doble)
--- suma.py
+++ suma.py
@@ -2,3 +2,4 @@ numero1=input("Ingrese el primer numero a")
 numero2=input("Ingrese el segundo numero a")
 suma=int(numero1)+int(numero2)
 print ("La suma de ",numero1, " +",numero2, " es",suma)
+ 
